96b6e4496016aa051378f017726ad6c0434390d1
Pedro Beneveli <pedro.beneveli@gmail.com>
2025-02-12 13:27:11 -0300

    Linked List functions implementation
    Tenho que verificar depois se o erase_linked_list funciona direito

--- linkedList.h
+++ linkedList.h
@@ -9,12 +9,12 @@ typedef struct node {
 } node_t;
 
 typedef struct linked_list {
-    int tam;
-    node_t* head, last;
+    int sz;
+    node_t *head, *last;
 } linked_list_t;
 
 // acho que nao vou precisar apagar processos, posso so ignorar os que acabarem
-node* create_node(process_t* p);
+node_t* create_node(process_t* p);
 linked_list_t* create_linked_list();
 void erase_linked_list(linked_list* list);
 void add_process_to_list(linked_list* list, process_t* p);
