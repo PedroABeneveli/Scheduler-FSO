3e1477f3b290363c8f9411046cbe82294b9e2ba7
Pedro Beneveli <pedro.beneveli@gmail.com>
2025-02-13 14:10:28 -0300

    Process structs

--- process.h
+++ process.h
@@ -1,8 +1,18 @@
 #ifndef PROCESS
 #define PROCESS
 
+typedef enum process_state {READY, RUNNING, BLOCKED} process_state_t;
+
+typedef struct process_context {
+    int eax, ecx, edx, ebx, esp, ebp, esi, edi, eip;
+} process_context_t;
+
 typedef struct process {
-    
+    process_context_t context;
+    process_state_t state;
+    int duration;   // for SJF
+    int arrival;    // relative arrival time used for FIFO
+    int time_left, in_processor;    // data needed for RR
 } process_t;
 
 #endif
