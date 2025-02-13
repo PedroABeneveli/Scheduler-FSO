2914fe3710c75089bc257bbff6466878cecef8bc
Pedro Beneveli <pedro.beneveli@gmail.com>
2025-02-13 14:37:27 -0300

    Process functions

---
+++ process.c
@@ -0,0 +1,53 @@
+#include "process.h"
+
+void process_init() {
+    curr_pid = 0;
+    process_count = 0;
+}
+
+process_t* create_process(int duration, int arrival) {
+    process_t *process = malloc(sizeof(process_t));
+
+    process->pid = curr_pid++;
+
+    // data for scheduling
+    process->arrival = arrival;
+    process->duration = duration;
+    process->time_left = duration;
+    process->time_running = 0;
+
+    process->context.eax = 0;
+    process->context.ebx = 0;
+    process->context.ecx = 0;
+    process->context.edx = 0;
+    process->context.esp = 0;
+    process->context.ebp = 0;
+    process->context.esi = 0;
+    process->context.edi = 0;
+    process->context.eip = 0;   // in the original code this is the base_address, but we wont use base address here
+
+    process->state = READY;
+
+    process_count++;
+
+    return process;
+}
+
+// type == 0 => FIFO
+// type == 1 => SJF
+// RR doesn't compare processes, it just goes to the next element in the list
+// returns true if p1 is going to be executed before p2
+int process_comp(process_t p1, process_t p2, int type) {
+    if (type == 0) {
+        if (p1.arrival < p2.arrival)
+            return true;
+        return false;
+    } else if (type == 1) {
+        if (p1.duration < p2.duration)
+            return true;
+        return false;
+    }
+
+    // invalid scheduling type
+    return -1;
+}
--- process.h
+++ process.h
@@ -1,6 +1,11 @@
 #ifndef PROCESS
 #define PROCESS
 
+#define true 1
+#define false 0
+
+int curr_pid, process_count;
+
 typedef enum process_state {READY, RUNNING, BLOCKED} process_state_t;
 
 typedef struct process_context {
@@ -8,11 +13,17 @@ typedef struct process_context {
 } process_context_t;
 
 typedef struct process {
+    int pid;
     process_context_t context;
     process_state_t state;
     int duration;   // for SJF
     int arrival;    // relative arrival time used for FIFO
-    int time_left, in_processor;    // data needed for RR
+    int time_left;  // how much time left this process needs to use the processor, used in RR
+    int time_running;    // time this process has been using the processor for RR
 } process_t;
+
+void process_init();
+process_t* create_process(int duration, int arrival);
+int process_comp(process_t p1, process_t p2, int type);
 
 #endif
